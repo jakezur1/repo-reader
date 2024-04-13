9f1ca1e8f305335fb048cfa420af3b46393e8287
Jake Zur <jakezur@umich.edu>
2024-04-12 22:03:30 -0400

    pushing for rith

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+// import AnimatedLinearGradient, {presetColors} from 'react-native-animated-linear-gradient'
 
 const Home = ({ navigate }: any) => {
   return (
       <div className={"w-300 h-300 rounded-2rem"}>
         <div className={'bg-transparent'}>
+          {/*<AnimatedLinearGradient customColors={presetColors.instagram} speed={4000}/>*/}
           <h1 className={'text-4xl m-3 font-bold text-purple-700'}>Welcome to RepoReader</h1>
         </div>
-        <button className={'flex-1 bg-white border-2 border-purple-700'} onClick={() => {
-        }}>Generate ReadMe</button>
+        <button className={'flex-1 bg-white border-2 border-purple-700'} onClick={() => {}}>Generate ReadMe</button>
       </div>
   );
 };

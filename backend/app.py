c6f288e6002d8543408e4a81fd07c503baf7f5ed
Aryaman Goenka <arygoenka@gmail.com>
2024-04-13 17:27:48 -0400

    cleaning up code

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, support_credentials=True)
-
 app.secret_key = 'repo'
 chat = None
 
@@ -28,13 +27,18 @@ def _build_cors_preflight_response():
 @app.route('/readme', methods=['GET', 'POST', 'OPTIONS'])
 def generation():
     method = request.method
+    
     if request.method == 'OPTIONS':
+        
         return _build_cors_preflight_response()
+   
     elif request.method == 'POST':
         query = request.get_json()
+
         username = query['username']
         repo = query['repository']
 
+        print(username)
         print(repo)
         #query = "my_list = [5, 2, 8, 3, 1] my_list.sort() print(my_list)"
 
@@ -44,21 +48,21 @@ def generation():
         #ouput = "dfd"
 
         # Open a text file in append mode
-        with open("ReadME.md", "a") as file:
-            file.write("")
-            file.write(output)
-        file.close()
-        
-        # response = {
-        #         "message" : ouput
-        #     }
-        # print(response)
-        # return jsonify(response)
+        # with open("ReadME.md", "a") as file:
+        #     file.write("")
+        #     file.write(output)
+        # file.close()
 
-        try:
-            return send_file('ReadME.md', as_attachment=True)
-        except Exception as e:
-            return str(e)
+        # try:
+        #     return send_file('ReadME.md', as_attachment=True)
+        # except Exception as e:
+        #     return str(e)
+
+        response = {
+                "message" : output
+            }
+        print(response)
+        return jsonify(response)
     
 @app.route('/start_chat', methods=['GET', 'POST'])
 def parserepo():
@@ -72,19 +76,11 @@ def parserepo():
        
         print(username)
         print(repo)
-
-        #query = "my_list = [5, 2, 8, 3, 1] my_list.sort() print(my_list)"
-
         full, req, sh = gh_api_caller.main(username, repo)
         
         global chat
         ouput, chat = gemini.readrepo(full)
-
-        response = {
-                "message" : ouput
-            }
-        print(response)
-        return jsonify(response)
+        return 
     
 @app.route('/chat', methods=['GET', 'POST'])
 def chat_on_code():
